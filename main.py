dc76cdb26749529d2d0cde378deb0e1bfd8bde57
Jonel Bacroya <Jonel_Bacroya@yahoo.com.ph>
2024-08-02 23:03:11 +0800

    update the return to send back the user details

--- main.py
+++ main.py
@@ -17,12 +17,15 @@ def get_user_details(user_id):
 
     return jsonify(user_detail), 200
 
-
 @app.route("/create-user", methods=["POST"])
 def create_user():
+    # this part should point to a database entity to save 
+    # the posted information
     user_details = request.get_json()
-    # jsonify(user_details)
-    return "success", 201
+
+    return jsonify(user_details), 201
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
