392af5b9a653f5bd4ecb67d7121d22aad842e933
Jonel Bacroya <Jonel_Bacroya@yahoo.com.ph>
2024-08-05 01:05:07 +0800

    added a success response if the create function was successfully executed

--- main.py
+++ main.py
@@ -2,31 +2,29 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/get-user-details/<user_id>")
 def get_user_details(user_id):
-    user_detail = {
-        "user_id": user_id,
-        "user_name": "Jonel-Bacroya",
-        "email_address": "test_email_add@yahoo.com.ph"
+    user_details = {
+        "username":"jonelbacroya",
+        "emailaddress": "test_emailaddress@yahoo.com",
+        "notes":"likes spicy food"
     }
 
-    additional_details = request.args.get("details")
+    if user_id:
+        return jsonify(user_details), 200
+    else:
+        return "invalid user", 404
 
-    if additional_details:
-        user_detail["additional_details"] = additional_details
-
-    return jsonify(user_detail), 200
-
-@app.route("/create-user", methods=["POST"])
+@app.route("/create-user", methods=["Post"])
 def create_user():
-    # this part should point to a database entity to save 
-    # the posted information
     user_details = request.get_json()
 
-    return jsonify(user_details), 201
-
-
-
+    if user_details:
+        user_details["created"] = "success!"
+        return jsonify(user_details),201
+    else:
+        return "failed to create!",404
+    
 if __name__ == "__main__":
     app.run(debug=True)
-
